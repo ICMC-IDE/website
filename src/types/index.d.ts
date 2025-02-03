3092faaf22d769e388e9793a611ff59fbf0c3e92
Thitav <thiptav@gmail.com>
2025-02-03 19:07:12 -0300

    Fixed imports

--- src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.css?inline" {
-  const content: string;
-  export default content;
-}
-declare module "*.html?template" {
-  const content: HTMLTemplateElement;
-  export default content;
-}
